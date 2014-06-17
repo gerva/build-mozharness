fa0672e169b70cf7e94147686614c8e81b752cef
Massimo Gervasini <mgervasini@mozilla.com>
2014-06-17 11:55:34 -0400

    added time_for_step

--- mozharness/mozilla/summary.py
+++ mozharness/mozilla/summary.py
@@ -1,6 +1,6 @@
 def per_locale_summary(func):
     """calls add_success/failure based on locale"""
-    def _decorator(self, *args, **kwargs):
+    def wrapper(self, *args, **kwargs):
         name = func.__name__
         locale = kwargs.get('locale', None)
         result = func(self, *args, **kwargs)
@@ -14,4 +14,28 @@ def per_locale_summary(func):
             message = 'failure: %s, locale = %s' %(name, locale)
             self.add_failure(locale, message)
         return result
-    return _decorator
+    return wrapper
+
+def time_for_step(func):
+    """prints out how long it took to run func"""
+    import time
+    def now():
+        return int(round(time.time()))
+
+    def wrapper(self, *args, **kwargs):
+        name = func.__name__
+        start_time = now()
+        locale = kwargs.get('locale', None)
+        try:
+            result = func(self, *args, **kwargs)
+        except:
+            delta = str(now() - start_time)
+            message = '%s failed in %s seconds (locale=%s)' %(name, delta, locale)
+            self.info(message)
+            raise
+        delta = (now() - start_time)
+        message = '%s completed in %s seconds (locale=%s)' %(name, delta, locale)
+        self.info(message)
+        return result
+    return wrapper
+
