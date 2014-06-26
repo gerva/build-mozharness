7d38b4c31f3dba413c5f740c199dbc43fcb42319
Massimo Gervasini <mgervasini@mozilla.com>
2014-06-26 11:54:50 -0400

    reverted balrog.py to mozilla/master version

--- mozharness/mozilla/updates/balrog.py
+++ mozharness/mozilla/updates/balrog.py
@@ -3,42 +3,34 @@ import os
 
 from mozharness.base.log import INFO
 
-
 # BalrogMixin {{{1
 class BalrogMixin(object):
-    def submit_balrog_updates(self, marfile, hash_type, appName, appVersion,
-                              platform, branch, buildid, complete_mar_url,
-                              release_type="nightly", ):
-        config = self.config
-        balrog_props = {}
-        balrog_props['hashType'] = hash_type
-        balrog_props['appName'] = appName
-        balrog_props['appVersion'] = appVersion
-        balrog_props['platform'] = platform
-        balrog_props['buildid'] = buildid
-        balrog_props['branch'] = branch
-        balrog_props['completeMarSize'] = self.query_filesize(marfile)
-        balrog_props['completeMarHash'] = self.query_sha512sum(marfile)
-        balrog_props['completeMarUrl'] = complete_mar_url
-
+    def submit_balrog_updates(self, release_type="nightly"):
+        c = self.config
         dirs = self.query_abs_dirs()
-        product = config["product"]
+        product = self.buildbot_config["properties"]["product"]
         props_path = os.path.join(dirs["base_work_dir"], "balrog_props.json")
         credentials_file = os.path.join(
-            dirs["base_work_dir"], config["balrog_credentials_file"]
+            dirs["base_work_dir"], c["balrog_credentials_file"]
         )
         submitter_script = os.path.join(
             dirs["abs_tools_dir"], "scripts", "updates", "balrog-submitter.py"
         )
+        self.set_buildbot_property(
+            "hashType", c.get("hash_type", "sha512"), write_to_file=True
+        )
+
         balrog_props = dict(properties=dict(chain(
-            balrog_props.items(),)))
+            self.buildbot_config["properties"].items(),
+            self.buildbot_properties.items(),
+        )))
         self.dump_config(props_path, balrog_props)
         cmd = [
             self.query_exe("python"),
             submitter_script,
             "--build-properties", props_path,
-            "--api-root", config["balrog_api_root"],
-            "--username", config["balrog_usernames"][product],
+            "--api-root", c["balrog_api_root"],
+            "--username", c["balrog_usernames"][product],
             "-t", release_type,
             "--credentials-file", credentials_file,
         ]
