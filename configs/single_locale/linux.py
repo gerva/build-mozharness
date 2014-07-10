ef57537319dc007096165f00f7fd94ac65d0c478
Massimo Gervasini <mgervasini@mozilla.com>
2014-07-10 16:06:50 -0700

    replaced current linux configuration with an older version

--- configs/single_locale/linux.py
+++ configs/single_locale/linux.py
@@ -5,23 +5,24 @@ EN_US_BINARY_URL = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/lates
 OBJDIR = "obj-l10n"
 MOZ_UPDATE_CHANNEL = "nightly"
 STAGE_SERVER = "dev-stage01.srv.releng.scl3.mozilla.com"
-# STAGE_SERVER = "stage.mozilla.org"
+#STAGE_SERVER = "stage.mozilla.org"
 STAGE_USER = "ffxbld"
 STAGE_SSH_KEY = "~/.ssh/ffxbld_dsa"
 AUS_SERVER = "dev-stage01.srv.releng.scl3.mozilla.com"
-# AUS_SERVER = "aus2-staging.mozilla.org"
+#AUS_SERVER = "aus2-staging.mozilla.org"
 AUS_USER = "ffxbld"
 AUS_SSH_KEY = "~/.ssh/ffxbld_dsa"
 AUS_UPLOAD_BASE_DIR = "/opt/aus2/incoming/2/Firefox"
 AUS_BASE_DIR = BRANCH + "/%(build_target)s/%(buildid)s/%(locale)s"
 CANDIDATES_URL = "http://ftp.mozilla.org/pub/mozilla.org/firefox/%s" % MOZ_UPDATE_CHANNEL
-PLATFORM = 'linux'
 config = {
+    'balrog_api_root': 'https://aus4-admin-dev.allizom.org',
+    "balrog_credentials_file": "oauth.txt",
+    'balrog_username': 'stage-ffxbld',
     "mozilla_dir": MOZILLA_DIR,
     "snippet_base_url": "http://example.com",
     "mozconfig": "%s/browser/config/mozconfigs/linux64/l10n-mozconfig" % MOZILLA_DIR,
     "binary_url": EN_US_BINARY_URL,
-    "platform": PLATFORM,
     "repos": [{
         "vcs": "hg",
         "repo": "https://hg.mozilla.org/mozilla-central",
@@ -43,11 +44,8 @@ config = {
         "LOCALE_MERGEDIR": "%(abs_merge_dir)s/",
         "MOZ_UPDATE_CHANNEL": MOZ_UPDATE_CHANNEL,
         "DIST": "%(abs_objdir)s",
-        "IS_NIGHTLY": "yes",
-        # "DIST": "%(abs_objdir)s",
-        # "LOCALE_MERGEDIR": "%(abs_merge_dir)s/",
+        "LOCALE_MERGEDIR": "%(abs_merge_dir)s/",
     },
-    "buildbot_json_path": "buildprops.json",
     "log_name": "single_locale",
     "objdir": OBJDIR,
     "js_src_dir": "js/src",
@@ -58,10 +56,11 @@ config = {
         "UPLOAD_USER": STAGE_USER,
         "UPLOAD_SSH_KEY": STAGE_SSH_KEY,
         "UPLOAD_HOST": STAGE_SERVER,
-        "POST_UPLOAD_CMD": "post_upload.py -b mozilla-central-l10n -p firefox -i %(buildid)s  --release-to-latest --release-to-dated",
+        #"POST_UPLOAD_CMD": "post_upload.py -b mozilla-central-android-l10n -p mobile -i %(buildid)s --release-to-latest --release-to-dated",
+        "POST_UPLOAD_CMD": "post_upload.py -b mozilla-central-l10n -p firefox -i %(buildid)s --release-to-latest --release-to-dated",
         "UPLOAD_TO_TEMP": "1"
     },
-    # l10n
+    #l10n
     "ignore_locales": ["en-US"],
     "l10n_dir": "l10n",
     "l10n_stage_dir": "dist/firefox/l10n-stage",
@@ -72,17 +71,17 @@ config = {
     "merge_locales": True,
     "clobber_file": 'CLOBBER',
 
-    # MAR
-    "previous_mar_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n",
-    "current_mar_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central",
+    #MAR
+    'previous_mar_url': 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n',
+    'current_mar_url': 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central',
     "previous_mar_dir": "previous",
     "current_mar_dir": "current",
-    "update_mar_dir": "dist/update",  # sure?
+    "update_mar_dir": "dist/update", # sure?
     "previous_mar_filename": "previous.mar",
     "current_work_mar_dir": "current.work",
     "package_base_dir": "dist/l10n-stage",
     "application_ini": "application.ini",
-    "buildid_section": "App",
+    "buildid_section": 'App',
     "buildid_option": "BuildID",
     "unpack_script": "tools/update-packaging/unwrap_full_update.pl",
     "incremental_update_script": "tools/update-packaging/make_incremental_update.sh",
@@ -92,21 +91,15 @@ config = {
     "mbsdiff": "mbsdiff",
     "candidates_base_url": CANDIDATES_URL,
     "partials_url": "%(base_url)s/latest-mozilla-central/",
-    "mar_tools_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/mar-tools/linux64/",
+    "mar_tools_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/mar-tools/linux/",
     "current_mar_filename": "firefox-%(version)s.en-US.linux-i686.complete.mar",
-    "complete_mar": "firefox-%(version)s.en-US.linux-x86_64.complete.mar",
+    "complete_mar": "firefox-%(version)s.en-US.linux-i686.complete.mar",
     "localized_mar": "firefox-%(version)s.%(locale)s.linux-i686.complete.mar",
     "partial_mar": "firefox-%(version)s.%(locale)s.linux-i686.partial.%(from_buildid)s-%(to_buildid)s.mar",
-    "installer_file": "firefox-%(version)s.en-US.linux-i686.tar.bz2",
-
-    # BALROG
-    "balrog_api_root": "https://aus4-admin-dev.allizom.org",
-    "balrog_credentials_file": "oauth.txt",
-    "balrog_username": "stage-ffxbld",
-    "appName": "Firefox",
-    "hashType": "sha512",
+    'installer_file': "firefox-%(version)s.en-US.linux-i686.tar.bz2",
 
     # AUS
+    "build_target": "Linux_x86-gcc3",
     "aus_server": AUS_SERVER,
     "aus_user": AUS_USER,
     "aus_ssh_key": AUS_SSH_KEY,
@@ -123,10 +116,10 @@ config = {
          'wireless-tools-devel.i686', 'libX11-devel.i686',
          'libXt-devel.i686', 'mesa-libGL-devel.i686',
          'gnome-vfs2-devel.i686', 'GConf2-devel.i686', 'wget',
-         'mpfr',  # required for system compiler
-         'xorg-x11-font*',  # fonts required for PGO
-         'imake',  # required for makedepend!?!
-         'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'gcc473_0moz1', 'yasm', 'ccache',  # <-- from releng repo
+         'mpfr', # required for system compiler
+         'xorg-x11-font*', # fonts required for PGO
+         'imake', # required for makedepend!?!
+         'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'gcc473_0moz1', 'yasm', 'ccache', # <-- from releng repo
          'valgrind',
          'pulseaudio-libs-devel.i686',
          'gstreamer-devel.i686', 'gstreamer-plugins-base-devel.i686',
@@ -151,10 +144,13 @@ config = {
          # Freetype packages need to be installed be version, because a newer
          # version is available, but we don't want it for Firefox builds.
          'freetype-2.3.11-6.el6_1.8.i686', 'freetype-devel-2.3.11-6.el6_1.8.i686',
-         'freetype-2.3.11-6.el6_1.8.x86_64', ],
-        'mock_files': [('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
-                       ('/home/cltbld/.hgrc', '/builds/.hgrc'),
-                       ('/home/cltbld/.boto', '/builds/.boto'),
-                       ('/builds/gapi.data', '/builds/gapi.data'),
-                       ('/tools/tooltool.py', '/builds/tooltool.py'), ],
+         'freetype-2.3.11-6.el6_1.8.x86_64',
+         ],
+         'mock_files': [
+                ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
+                ('/home/cltbld/.hgrc', '/builds/.hgrc'),
+                ('/home/cltbld/.boto', '/builds/.boto'),
+                ('/builds/gapi.data', '/builds/gapi.data'),
+                ('/tools/tooltool.py', '/builds/tooltool.py'),
+         ],
 }
