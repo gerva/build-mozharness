d9a470883790e02bd220121babfe3e9287dc4a11
Massimo Gervasini <mgervasini@mozilla.com>
2014-07-10 16:23:29 -0700

    added platform option in configuration

--- configs/single_locale/linux.py
+++ configs/single_locale/linux.py
@@ -15,6 +15,7 @@ AUS_SSH_KEY = "~/.ssh/ffxbld_dsa"
 AUS_UPLOAD_BASE_DIR = "/opt/aus2/incoming/2/Firefox"
 AUS_BASE_DIR = BRANCH + "/%(build_target)s/%(buildid)s/%(locale)s"
 CANDIDATES_URL = "http://ftp.mozilla.org/pub/mozilla.org/firefox/%s" % MOZ_UPDATE_CHANNEL
+PLATFROM = 'linux'
 config = {
     'balrog_api_root': 'https://aus4-admin-dev.allizom.org',
     "balrog_credentials_file": "oauth.txt",
@@ -23,6 +24,7 @@ config = {
     "snippet_base_url": "http://example.com",
     "mozconfig": "%s/browser/config/mozconfigs/linux64/l10n-mozconfig" % MOZILLA_DIR,
     "binary_url": EN_US_BINARY_URL,
+    "platform": PLATFORM,
     "repos": [{
         "vcs": "hg",
         "repo": "https://hg.mozilla.org/mozilla-central",
