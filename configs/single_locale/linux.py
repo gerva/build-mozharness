2ba66ca4a42653f0305e8ecb0af1c65496aa4342
Massimo Gervasini <mgervasini@mozilla.com>
2014-07-10 17:54:53 -0700

    fixed typo

--- configs/single_locale/linux.py
+++ configs/single_locale/linux.py
@@ -15,7 +15,7 @@ AUS_SSH_KEY = "~/.ssh/ffxbld_dsa"
 AUS_UPLOAD_BASE_DIR = "/opt/aus2/incoming/2/Firefox"
 AUS_BASE_DIR = BRANCH + "/%(build_target)s/%(buildid)s/%(locale)s"
 CANDIDATES_URL = "http://ftp.mozilla.org/pub/mozilla.org/firefox/%s" % MOZ_UPDATE_CHANNEL
-PLATFROM = 'linux'
+PLATFORM= 'linux'
 config = {
     'balrog_api_root': 'https://aus4-admin-dev.allizom.org',
     "balrog_credentials_file": "oauth.txt",
