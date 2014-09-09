1d8956d7d9e96c40e970885966f25d2d9c9b3f6b
Massimo Gervasini <mgervasini@mozilla.com>
2014-09-09 13:20:06 +0200

    removed branch_repo

--- scripts/desktop_l10n.py
+++ scripts/desktop_l10n.py
@@ -51,8 +51,7 @@ PyMakeIgnoreList = [
 # it's a list of values that are already known before starting a build
 configuration_tokens = ('branch',
                         'platform',
-                        'en_us_binary_url',
-                        'branch_repo')
+                        'en_us_binary_url',)
 # some other values such as "%(version)s", "%(buildid)s", ...
 # are defined at run time and they cannot be enforced in the _pre_config_lock
 # phase
