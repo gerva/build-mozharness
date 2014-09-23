5dceea257dfcb42877231d50c1e92ec28f0b1a96
Massimo Gervasini <mgervasini@mozilla.com>
2014-09-23 13:20:28 +0200

    disabled aus_* and AUS_* from staging.py - just a test

--- configs/single_locale/staging.py
+++ configs/single_locale/staging.py
@@ -1,11 +1,12 @@
 UPLOAD_SERVER = "dev-stage01.srv.releng.scl3.mozilla.com"
 UPLOAD_USER = "ffxbld"
 UPLOAD_SSH_KEY = "~/.ssh/ffxbld_dsa"
-AUS_USER = "ffxbld"
-AUS_SSH_KEY = "~/.ssh/ffxbld_dsa"
-AUS_UPLOAD_BASE_DIR = "/opt/aus2/incoming/2/Firefox"
-AUS_BASE_DIR = "%(branch)s/%(update_platform)s/%(buildid)s/%(locale)s"
-AUS_SERVER = "blah"
+# disabled for testing
+#AUS_USER = "ffxbld"
+#AUS_SSH_KEY = "~/.ssh/ffxbld_dsa"
+#AUS_UPLOAD_BASE_DIR = "/opt/aus2/incoming/2/Firefox"
+#AUS_BASE_DIR = "%(branch)s/%(update_platform)s/%(buildid)s/%(locale)s"
+#AUS_SERVER = "blah"
 
 config = {
     "upload_env": {
@@ -16,9 +17,9 @@ config = {
         "UPLOAD_TO_TEMP": "1"
     },
     # AUS
-    "aus_server": AUS_SERVER,
-    "aus_user": AUS_USER,
-    "aus_ssh_key": AUS_SSH_KEY,
-    "aus_upload_base_dir": AUS_UPLOAD_BASE_DIR,
-    "aus_base_dir": AUS_BASE_DIR,
+#    "aus_server": AUS_SERVER,
+#    "aus_user": AUS_USER,
+#    "aus_ssh_key": AUS_SSH_KEY,
+#    "aus_upload_base_dir": AUS_UPLOAD_BASE_DIR,
+#    "aus_base_dir": AUS_BASE_DIR,
 }
